5b06d0c6ff2934d0da7d6946a154df98c5f78ae6
Anton <mtvv2005@yandex.by>
2024-11-01 19:24:52 +0300

    small changes

--- src/main/java/com/javarush/jira/common/internal/config/AppConfig.java
+++ src/main/java/com/javarush/jira/common/internal/config/AppConfig.java
@@ -8,8 +8,6 @@ import com.javarush.jira.common.util.JsonUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
